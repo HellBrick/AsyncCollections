e80f7caa12b8b030e0faaed4a025461a32e70591
HellBrick <hellbrick@gmail.com>
2014-10-06 20:16:28 +0400

    Added AsyncQueue prototype

---
+++ AsyncCollections.Test/AsyncCollectionTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HellBrick.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HellBrick.Collections.Test
+{
+	[TestClass]
+	public class AsyncCollectionTest
+	{
+		private AsyncQueue<int> _collection;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_collection = new AsyncQueue<int>();
+		}
+
+		[TestMethod]
+		public void TakingItemFromNonEmptyCollectionCompletesImmediately()
+		{
+			_collection.Add( 42 );
+			var itemTask = _collection.TakeAsync();
+			Assert.IsTrue( itemTask.IsCompleted );
+			Assert.AreEqual( 42, itemTask.Result );
+		}
+
+		[TestMethod]
+		public void AddingItemCompletesPendingTask()
+		{
+			var itemTask = _collection.TakeAsync();
+			Assert.IsFalse( itemTask.IsCompleted );
+
+			_collection.Add( 42 );
+			Assert.IsTrue( itemTask.IsCompleted );
+			Assert.AreEqual( 42, itemTask.Result );
+		}
+
+		[TestMethod]
+		public void CancelledTakeCancelsTask()
+		{
+			CancellationTokenSource cancelSource = new CancellationTokenSource();
+			var itemTask = _collection.TakeAsync( cancelSource.Token );
+			cancelSource.Cancel();
+			Assert.IsTrue( itemTask.IsCanceled );
+
+			_collection.Add( 42 );
+			Assert.AreEqual( 1, _collection.Count );
+			Assert.AreEqual( 0, _collection.AwaiterCount );
+		}
+
+		[TestMethod]
+		[Timeout( 30000 )]
+		public async Task RandomMultithreadingOperationsDontCrash()
+		{
+			int itemsTaken = 0;
+			int itemCount = 100;
+			int producerThreads = 4;
+			int consumerThreads = 2;
+			CancellationTokenSource cancelSource = new CancellationTokenSource();
+
+			List<Task> consumerTasks = new List<Task>();
+
+			for ( int i = 0; i < consumerThreads; i++ )
+			{
+				int consumerID = i;
+				var consumerTask = Task.Run(
+					async () =>
+					{
+						while ( !cancelSource.IsCancellationRequested || itemsTaken < itemCount * producerThreads )
+						{
+							int item = await _collection.TakeAsync();
+							Interlocked.Increment( ref itemsTaken );
+							Debug.WriteLine( "{0} ( - {1} by {2} )", _collection, item, consumerID );
+						}
+					} );
+
+				consumerTasks.Add( consumerTask );
+			}
+
+			List<Task> producerTasks = new List<Task>();
+
+			for ( int i = 0; i < producerThreads; i++ )
+			{
+				int producerID = i;
+
+				var producerTask = Task.Run(
+					() =>
+					{
+						for ( int j = 0; j < itemCount; j++ )
+						{
+							int item = producerID * itemCount + j;	//	some kind of a unique item ID
+							_collection.Add( item );
+							Debug.WriteLine( _collection );
+						}
+					} );
+
+				producerTasks.Add( producerTask );
+			}
+
+			await Task.WhenAll( producerTasks );
+			cancelSource.Cancel();
+
+			await Task.WhenAll( consumerTasks );
+			Assert.AreEqual( 0, _collection.Count );
+		}
+	}
+}
---
+++ AsyncCollections/AsyncQueue.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HellBrick.Collections
+{
+	/// <summary>
+	/// Represents a thread-safe queue that allows asynchronous consuming.
+	/// </summary>
+	/// <typeparam name="T">The type of the items contained in the collection.</typeparam>
+	public class AsyncQueue<T>: IEnumerable<T>, System.Collections.ICollection
+	{
+		private ConcurrentQueue<T> _itemQueue = new ConcurrentQueue<T>();
+		private ConcurrentQueue<TaskCompletionSource<T>> _awaiterQueue = new ConcurrentQueue<TaskCompletionSource<T>>();
+
+		//	_queueBalance < 0 means there are free awaiters and not enough items.
+		//	_queueBalance > 0 means the opposite is true.
+		private long _queueBalance = 0;
+
+		#region Properties
+
+		public int AwaiterCount
+		{
+			get { return _awaiterQueue.Count; }
+		}
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// Adds an item to the collection.
+		/// </summary>
+		/// <param name="item">The item to add to the collection.</param>
+		public void Add( T item )
+		{
+			while ( !TryAdd( item ) ) ;
+		}
+
+		/// <summary>
+		/// Tries to add an item to the collection.
+		/// May fail if an awaiter that's supposed to receive the item is cancelled. If this is the case, the TryAdd() method must be called again.
+		/// </summary>
+		/// <param name="item">The item to add to the collection.</param>
+		/// <returns>True if the item was added to the collection; false if the awaiter was cancelled and the operation must be retried.</returns>
+		private bool TryAdd( T item )
+		{
+			long balanceAfterCurrentItem = Interlocked.Increment( ref _queueBalance );
+
+			if ( balanceAfterCurrentItem > 0 )
+			{
+				//	Items are dominating, so we can safely add a new item to the queue.
+				_itemQueue.Enqueue( item );
+				return true;
+			}
+			else
+			{
+				//	There's at least one awaiter available or being added as we're speaking, so we're giving the item to it.
+
+				TaskCompletionSource<T> awaiter;
+				SpinWait spin = new SpinWait();
+
+				while ( !_awaiterQueue.TryDequeue( out awaiter ) )
+					spin.SpinOnce();
+
+				//	Returns false if the cancellation occurred earlier.
+				return awaiter.TrySetResult( item );
+			}
+		}
+
+		/// <summary>
+		/// Removes and returns an item from the collection in an asynchronous manner.
+		/// </summary>
+		public Task<T> TakeAsync()
+		{
+			return TakeAsync( CancellationToken.None );
+		}
+
+		/// <summary>
+		/// Removes and returns an item from the collection in an asynchronous manner.
+		/// </summary>
+		public Task<T> TakeAsync( CancellationToken cancellationToken )
+		{
+			long balanceAfterCurrentAwaiter = Interlocked.Decrement( ref _queueBalance );
+
+			if ( balanceAfterCurrentAwaiter < 0 )
+			{
+				//	Awaiters are dominating, so we can safely add a new awaiter to the queue.
+
+				var taskSource = new TaskCompletionSource<T>();
+				_awaiterQueue.Enqueue( taskSource );
+
+				cancellationToken.Register(
+					state =>
+					{
+						//	It's enough to call TrySetCancelled() here.
+						//	The balance correction will be taken care of in the Add() method that will retreive the current awaiter from the queue.
+						TaskCompletionSource<T> awaiter = state as TaskCompletionSource<T>;
+						awaiter.TrySetCanceled();
+					},
+					taskSource,
+					useSynchronizationContext : false );
+
+				return taskSource.Task;
+			}
+			else
+			{
+				//	There's at least one item available or being added, so we're returning it directly.
+
+				T item;
+				SpinWait spin = new SpinWait();
+
+				while ( !_itemQueue.TryDequeue( out item ) )
+					spin.SpinOnce();
+
+				return Task.FromResult( item );
+			}
+		}
+
+		public override string ToString()
+		{
+			return String.Format( "Count = {0}, Awaiters = {1}", Count, AwaiterCount );
+		}
+
+		#endregion
+
+		#region IEnumerable<T> Members
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			return _itemQueue.GetEnumerator();
+		}
+
+		#endregion
+
+		#region IEnumerable Members
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			return _itemQueue.GetEnumerator();
+		}
+
+		#endregion
+
+		#region ICollection Members
+
+		public int Count
+		{
+			get { return _itemQueue.Count; }
+		}
+
+		public void CopyTo( Array array, int index )
+		{
+			( _itemQueue as System.Collections.ICollection ).CopyTo( array, index );
+		}
+
+		bool System.Collections.ICollection.IsSynchronized
+		{
+			get { return false; }
+		}
+
+		object System.Collections.ICollection.SyncRoot
+		{
+			get { throw new NotSupportedException(); }
+		}
+
+		#endregion
+	}
+}
