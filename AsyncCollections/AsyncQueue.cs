aeb33d9ecc4339a23be5b64f4c9e02774f5201e2
HellBrick <hellbrick@gmail.com>
2014-10-06 20:48:28 +0400

    Derived AsyncQueue and AsyncStack from AsyncCollection

---
+++ AsyncCollections/AsyncQueue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellBrick.Collections
+{
+	/// <summary>
+	/// Represents a thread-safe queue that allows asynchronous consuming.
+	/// </summary>
+	/// <typeparam name="TItem">The type of the items contained in the queue.</typeparam>
+	public class AsyncQueue<T>: AsyncCollection<T, ConcurrentQueue<T>>
+	{
+	}
+}
