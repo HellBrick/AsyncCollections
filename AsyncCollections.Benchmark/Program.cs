7362579fe8dda4d27e5d0d2fa6a3d9c45a78e2af
HellBrick <hellbrick@gmail.com>
2014-10-17 22:20:46 +0400

    Added a benchmark for AsyncQueue

---
+++ AsyncCollections.Benchmark/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellBrick.AsyncCollections.Benchmark
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			var competition = new AsyncQueueBenchmark();
+			competition.Run();
+		}
+	}
+}
