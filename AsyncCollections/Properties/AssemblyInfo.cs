55248c652b294357d9c8c1c4fa858387aa73ece4
HellBrick <hellbrick@gmail.com>
2014-10-18 19:04:30 +0400

    Added .nuspec file and adjusted the assembly properties

--- AsyncCollections/Properties/AssemblyInfo.cs
+++ AsyncCollections/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "AsyncCollections" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "This package contains a set of non-blocking threadsafe collections designed to be used asynchronously." )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyCompany( "HellBrick" )]
 [assembly: AssemblyProduct( "AsyncCollections" )]
 [assembly: AssemblyCopyright( "Copyright ©  2014" )]
 [assembly: AssemblyTrademark( "" )]
