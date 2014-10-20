e9a972f25ce640ffd3fe68b9656cf9f160ba94ac
Pavel Mayorov <Pavel.Mayorov@centre-it.com>
2014-10-20 16:41:44 +0600

    Added benchmark for System.Threading.Tasks.Dataflow.BufferBlock

---
+++ AsyncCollections.Benchmark/TplDataflowAdapter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using HellBrick.Collections;
+
+namespace HellBrick.AsyncCollections.Benchmark
+{
+	class TplDataflowAdapter<T>: IAsyncCollection<T>
+	{
+		private readonly BufferBlock<T> buffer = new BufferBlock<T>();
+
+		public int AwaiterCount
+		{
+			get { throw new NotSupportedException(); }
+		}
+
+		public void Add( T item )
+		{
+			buffer.Post( item );
+		}
+
+		public Task<T> TakeAsync( CancellationToken cancellationToken )
+		{
+			return buffer.ReceiveAsync( cancellationToken );
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void CopyTo( Array array, int index )
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Count
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public bool IsSynchronized
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public object SyncRoot
+		{
+			get { throw new NotImplementedException(); }
+		}
+	}
+}
