788e2714a38654749d4318f920bf11e11814c5e2
HellBrick <hellbrick@gmail.com>
2014-10-25 17:44:13 +0400

    Added AsyncBoundedPriorityQueue tests

---
+++ AsyncCollections.Test/AsyncBoundedPriorityQueueTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HellBrick.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HellBrick.Collections.Test
+{
+	[TestClass]
+	public class AsyncBoundedPriorityQueueTest: AsyncCollectionTest<AsyncBoundedPriorityQueue<int>>
+	{
+		protected override AsyncBoundedPriorityQueue<int> CreateCollection()
+		{
+			return new AsyncBoundedPriorityQueue<int>( 2 );
+		}
+
+		[TestMethod]
+		public async Task ReturnsLowPriorityIfNoHighPriorityIsAvailable()
+		{
+			Collection.Add( 42, 1 );
+			var result = await Collection.TakeAsync();
+			Assert.AreEqual( 42, result );
+		}
+
+		[TestMethod]
+		public async Task RespectsPriority()
+		{
+			Collection.Add( 42, 0 );
+			Collection.Add( 999, 1 );
+
+			var result = await Collection.TakeAsync();
+			Assert.AreEqual( 42, result );
+		}
+	}
+}
