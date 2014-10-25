a782369c0b2eb963cba8a4e9b776a5d4e843786e
HellBrick <hellbrick@gmail.com>
2014-10-25 19:50:08 +0400

    Bumped the version and adjusted the description

--- AsyncCollections/Properties/AssemblyInfo.cs
+++ AsyncCollections/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "AsyncCollections" )]
-[assembly: AssemblyDescription( "This package contains a set of non-blocking thread-safe collections designed to be used asynchronously." )]
+[assembly: AssemblyDescription( "This package contains a set of lock-free thread-safe collections designed to be used asynchronously." )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "HellBrick" )]
 [assembly: AssemblyProduct( "AsyncCollections" )]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.1.0.0" )]
-[assembly: AssemblyFileVersion( "1.1.0.0" )]
+[assembly: AssemblyVersion( "1.2.0.0" )]
+[assembly: AssemblyFileVersion( "1.2.0.0" )]
