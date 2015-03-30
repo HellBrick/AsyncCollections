d40989b02787cc78be41705ad2aa2bfe0452bd75
Pavel Mayorov <mayorovp@gmail.com>
2015-03-30 21:18:47 +0500

    Сделал грязный хак более совместимым с Mono
    https://github.com/HellBrick/AsyncCollections/issues/4

--- AsyncCollections/Internal/TaskExtensions.cs
+++ AsyncCollections/Internal/TaskExtensions.cs
@@ -10,28 +10,10 @@ namespace HellBrick.Collections.Internal
 {
 	internal static class TaskExtensions
 	{
-		private static readonly Action<Task> _stateSetter = GenerateStateSetter();
-
-		private static Action<Task> GenerateStateSetter()
+		public static async Task<T> WithThreadAbortedFlag<T>( this Task<T> task )
 		{
-			const int taskStateThreadWasAborted = 0x8000000;
-			FieldInfo stateField = typeof( Task ).GetTypeInfo().GetDeclaredField( "m_stateFlags" );
-
-			ParameterExpression taskParameter = Expression.Parameter( typeof( Task ) );
-			var setterExpression =
-				Expression.Assign(
-					Expression.Field( taskParameter, stateField ),
-					Expression.Or(
-						Expression.Field( taskParameter, stateField ),
-						Expression.Constant( taskStateThreadWasAborted ) ) );
-
-			return Expression.Lambda<Action<Task>>( setterExpression, taskParameter ).Compile();
-		}
-
-		public static Task<T> WithThreadAbortedFlag<T>( this Task<T> task )
-		{
-			_stateSetter( task );
-			return task;
+			await Task.Yield();
+			return await task;
 		}
 	}
 }
