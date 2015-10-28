9079e4843ae93df7c31e15e10a230cd82b1a4222
HellBrick <hellbrick@webkontrol.ru>
2015-10-28 12:32:15 +0300

    Implemented AnyResult: IEquatable

--- AsyncCollections/AnyResult.cs
+++ AsyncCollections/AnyResult.cs
@@ -9,7 +9,7 @@ namespace HellBrick.Collections
 	/// <summary>
 	/// Represents an item retrieved from one of the asynchronous collections.
 	/// </summary>
-	public struct AnyResult<T>
+	public struct AnyResult<T> : IEquatable<AnyResult<T>>
 	{
 		public AnyResult( T value, int collectionIndex )
 		{
@@ -26,5 +26,23 @@ namespace HellBrick.Collections
 		/// Gets the index of the collection the item was retrieved from.
 		/// </summary>
 		public int CollectionIndex { get; }
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				const int prime = -1521134295;
+				int hash = 12345701;
+				hash = hash * prime + EqualityComparer<T>.Default.GetHashCode( Value );
+				hash = hash * prime + EqualityComparer<int>.Default.GetHashCode( CollectionIndex );
+				return hash;
+			}
+		}
+
+		public bool Equals( AnyResult<T> other ) => EqualityComparer<T>.Default.Equals( Value, other.Value ) && EqualityComparer<int>.Default.Equals( CollectionIndex, other.CollectionIndex );
+		public override bool Equals( object obj ) => obj is AnyResult<T> && Equals( (AnyResult<T>) obj );
+
+		public static bool operator ==( AnyResult<T> x, AnyResult<T> y ) => x.Equals( y );
+		public static bool operator !=( AnyResult<T> x, AnyResult<T> y ) => !x.Equals( y );
 	}
 }
