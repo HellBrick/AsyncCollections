b793bc0e9b6786c8e05f899dd09fdc81f712eb43
HellBrick <hellbrick@outlook.com>
2016-05-08 12:18:34 +0300

    Added Collection.Should().BeEqualTo() extension

---
+++ AsyncCollections.Test/Assertions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Collections;
+
+namespace HellBrick.Collections.Test
+{
+	public static class Assertions
+	{
+		/// <summary>
+		/// Expects the current collection to contain all elements of <paramref name="expected"/> in that exact order.
+		/// </summary>
+		public static AndConstraint<GenericCollectionAssertions<T>> BeEqualTo<T>( this GenericCollectionAssertions<T> collectionShould, IReadOnlyList<T> expected )
+		{
+			AndConstraint<GenericCollectionAssertions<T>> andConstraint = collectionShould.HaveSameCount( expected );
+
+			for ( int i = 0; i < expected.Count; i++ )
+				andConstraint = collectionShould.HaveElementAt( i, expected[ i ] );
+
+			return andConstraint;
+		}
+	}
+}
