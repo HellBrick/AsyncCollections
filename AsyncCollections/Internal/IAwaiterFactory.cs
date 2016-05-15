2009bb75dcd33786fc38e24f6fdf0811e43735ea
HellBrick <hellbrick@outlook.com>
2016-05-15 19:55:11 +0300

    Removed unused class

--- AsyncCollections/Internal/IAwaiterFactory.cs
+++ AsyncCollections/Internal/IAwaiterFactory.cs
@@ -10,27 +10,4 @@ namespace HellBrick.Collections.Internal
 	{
 		IAwaiter<T> CreateAwaiter();
 	}
-
-	internal struct InstanceAwaiterFactory<T> : IAwaiterFactory<T>, IEquatable<InstanceAwaiterFactory<T>>
-	{
-		private readonly IAwaiter<T> _awaiter;
-
-		public InstanceAwaiterFactory( IAwaiter<T> awaiter )
-		{
-			_awaiter = awaiter;
-		}
-
-		public IAwaiter<T> CreateAwaiter() => _awaiter;
-
-		#region IEquatable<InstanceAwaiterFactory<T>>
-
-		public override int GetHashCode() => EqualityComparer<IAwaiter<T>>.Default.GetHashCode( _awaiter );
-		public bool Equals( InstanceAwaiterFactory<T> other ) => EqualityComparer<IAwaiter<T>>.Default.Equals( _awaiter, other._awaiter );
-		public override bool Equals( object obj ) => obj is InstanceAwaiterFactory<T> && Equals( (InstanceAwaiterFactory<T>) obj );
-
-		public static bool operator ==( InstanceAwaiterFactory<T> x, InstanceAwaiterFactory<T> y ) => x.Equals( y );
-		public static bool operator !=( InstanceAwaiterFactory<T> x, InstanceAwaiterFactory<T> y ) => !x.Equals( y );
-
-		#endregion
-	}
 }
