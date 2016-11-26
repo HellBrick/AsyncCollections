c883e35be4861d3c7539a57d40511792c02b131a
HellBrick <hellbrick@outlook.com>
2016-11-26 19:44:22 +0300

    [debug]

--- AsyncCollections.Benchmark/Program.cs
+++ AsyncCollections.Benchmark/Program.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Running;
+using HellBrick.Collections;
 
 namespace HellBrick.AsyncCollections.Benchmark
 {
 	internal class Program
 	{
-		private static void Main( string[] args ) => new BenchmarkSwitcher( EnumerateBenchmarks().ToArray() ).Run();
+		private static void Main( string[] args )
+		{
+			var q = new ConcurrentPriorityQueue<int, int>( 10, 4.0 );
+			q.TryAdd( new KeyValuePair<int, int>( 5, 5 ) );
+			q.TryAdd( new KeyValuePair<int, int>( 4, 4 ) );
+			q.TryAdd( new KeyValuePair<int, int>( 1, 1 ) );
+			q.TryAdd( new KeyValuePair<int, int>( 3, 3 ) );
+			q.TryAdd( new KeyValuePair<int, int>( 2, 2 ) );
+
+			//new BenchmarkSwitcher( EnumerateBenchmarks().ToArray() ).Run();
+		}
 
 		private static IEnumerable<Type> EnumerateBenchmarks()
 		{
